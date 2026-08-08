0d2d003b699f628285d210329f6b6d4c00d42134
Haider Mustafa <haidersyedmustafa43@gmail.com>
2026-08-08 23:37:38 +0400

    Update sandbox.c

--- Level_1/sandbox/sandbox.c
+++ Level_1/sandbox/sandbox.c
@@ -36,7 +36,6 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 #include <string.h>
 
 void alarm_handler(int sig)
@@ -47,7 +46,7 @@ void alarm_handler(int sig)
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
     struct sigaction sa;
-    pid_t pid;
+    int pid;
     int status;
     int code;
 
